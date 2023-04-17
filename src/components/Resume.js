e4b9f698a91a2d6c885244205605fc657ad55907
Sam Ayoub <m22386@live.com>
2023-04-16 17:37:15 -0700

    v1.1

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,26 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-
+import Carousel from 'react-multi-carousel';
+import Zmage from 'react-zmage';
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 6
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 class Resume extends Component {
   getRandomColor() {
     let letters = '0123456789ABCDEF';
@@ -43,17 +63,33 @@ class Resume extends Component {
 
     const skills = this.props.data.skills.map((skills) => {
       const backgroundColor = this.getRandomColor();
-      const className = 'bar-expand ' + skills.name.toLowerCase();
+      const className = 'bar-expand  border' + skills.name.toLowerCase();
       const width = skills.level;
 
       return (
         <li key={skills.name}>
-          <span style={{ width, backgroundColor }} className={className}></span>
-          <em>{skills.name}</em>
+          <div style={{ width, backgroundColor }} className={className}>
+            <div className="p-2 text-dark ">{width}</div>
+          </div>
+          <em className="text-dark" style={{ fontSize: '10px' }}>
+            {skills.name}
+          </em>
         </li>
       );
     });
+    const certs = this.props.data.certs.map(function (cert) {
+      let certImage = cert.image;
 
+      return (
+        <a href={cert.source} key={cert.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <Zmage alt={cert.title} src={certImage} />
+            <div style={{ textAlign: 'center' }}>{cert.title}</div>
+            <div style={{ textAlign: 'center' }}>Certification link</div>
+          </div>
+        </a>
+      );
+    });
     return (
       <section id="resume">
         <div>
@@ -70,7 +106,18 @@ class Resume extends Component {
             </div>
           </div>
         </div>
-
+        <div>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Certifications</span>
+              </h1>
+            </div>
+            <div className="nine columns main-col">
+              <Carousel responsive={responsive}>{certs}</Carousel>
+            </div>
+          </div>
+        </div>
         <div>
           <div className="row work">
             <div className="three columns header-col">
